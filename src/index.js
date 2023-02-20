cc53c3ef1cd57a166562044da2b8764e4c5d17fc
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-02-20 16:44:05 -0300

    correção de rotas

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter hashType="slash">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
