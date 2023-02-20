547ca9b19813db724e0a8b0de4e2881823e1dbce
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-02-20 16:25:19 -0300

    correção de rotas

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
       <StyledGlobal />
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/my-pokedex" component={ Home } />
         <Route exact path="/details/:name" component={ PokemonDetails } />
         <Route exact path="/favorites" component={ FavoritePokemons } />
         <Route exact path="/about" component={ About } />
