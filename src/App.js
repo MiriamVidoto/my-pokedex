fd7b906aadeb4e7f3d9fc59349a5e865ee2f9044
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-02-20 16:49:19 -0300

    correção de rotas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import PokedexProvider from './context/PokedexProvider';
 import About from './pages/About';
 import FavoritePokemons from './pages/FavoritesPokemons';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 import PokemonDetails from './pages/PokemonDetails';
 import { StyledGlobal } from './styleds/StyledGlobal';
 
@@ -14,10 +13,10 @@ export default function App() {
       <StyledGlobal />
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/my-pokedex" component={ Home } />
         <Route exact path="/details/:name" component={ PokemonDetails } />
         <Route exact path="/favorites" component={ FavoritePokemons } />
         <Route exact path="/about" component={ About } />
-        <Route component={ NotFound } />
       </Switch>
     </PokedexProvider>
 
